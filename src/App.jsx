4c53ca1b1187e286e89366aabc60a62f97d04797
Brianojunga <brianojunga3@gmail.com>
2025-04-02 02:40:03 +0300

    contain react hooks and usestates

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./header";
+import Buttons from "./Buttons";
+import Extensions from "./extensions";
+
+function App() {
+  const [extensions, setExtension] = useState([]);
+  const [all, setAll] = useState(true);
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((response) => setExtension(response));
+  }, []);
+
+  return (
+    <main>
+      <Header />
+      <section>
+        <Buttons
+          all={all}
+          setAll={setAll}
+          active={active}
+          setActive={setActive}
+        />
+        <Extensions
+          extensions={extensions}
+          setExtension={setExtension}
+          active={active}
+          all={all}
+        />
+      </section>
+    </main>
+  );
+}
+
+export default App;
